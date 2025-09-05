5818bcc59a7f6561254422920fcdae6bf09b5a03
Fredrik Karlsson <fredrikkarlsson320@gmail.com>
2025-09-05 10:42:39 +0200

    handle templates

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ const form = document.getElementById("basicForm");
 const amount = form.elements.amount;
 const result = form.elements.result;
 
+async function loadPart(file, container) {
+  const res = await fetch(file);
+  const text = await res.text();
+
+  const tempDiv = document.createElement("div");
+  tempDiv.innerHTML = text;
+  const template = tempDiv.querySelector("template");
+
+  if (template) {
+    const templateContent = template.content.cloneNode(true);
+    return container.appendChild(templateContent);
+  } else {
+    return container.insertAdjacentHTML("beforeend", text);
+  }
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const data = new FormData(e.target);
@@ -17,3 +33,12 @@ form.addEventListener("input", () => {
     result.value = amount.value;
   }
 });
+
+/* ------------------------------------- */
+/* Laddar in delar av sidan från olika filer */
+
+(async function () {
+  // await loadPart("section1.html", form);
+})();
+
+/* ------------------------------------- */
